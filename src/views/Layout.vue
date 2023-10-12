9adda5f738c5bb5a27351d100b35d051abf8165d
gaomengying <gaomengying@topichina.com.cn>
2023-10-12 11:09:03 +0800

    提交-头部标题通栏

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,4 +1,14 @@
+<!--
+ * @Descripttion: layout
+ * @version: 1.0
+ * @Author: G
+ * @Date: 2023-09-22 12:25:05
+ * @LastEditors: G
+ * @LastEditTime: 2023-10-11 19:15:54
+-->
 <template>
+  <AppTopbar />
+  <AppHeader />
   <nav>顶部通栏</nav>
   <header>头部</header>
   <div class="main">
@@ -8,8 +18,12 @@
 </template>
 
 <script>
+import AppTopbar from '@/components/app-topbar.vue'
+import AppHeader from '@/components/app-header.vue'
+
 export default {
-  name: 'Layout'
+  name: 'Layout',
+  components: { AppTopbar, AppHeader }
 }
 </script>
 
